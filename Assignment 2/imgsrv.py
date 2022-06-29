8f6d7b03ff133e659662f4fccce8ba29b7fa1f1b
Jessi Frenzel <8403386+starfrenzy@users.noreply.github.com>
2022-06-29 00:54:22 -0500

    Added files to the directory. (Two .txt files and a folder with images.)

--- Assignment 2/imgsrv.py
+++ Assignment 2/imgsrv.py
@@ -2,7 +2,7 @@ import time
 
 while True:
     time.sleep(1)
-    with open("image-service.txt", "a", encoding="utf-8") as f:
+    with open("image-service.txt", "r+", encoding="utf-8") as f:
         read_data = f.read()
         if read_data.isnumeric():
             our_num = float(read_data)
--- Assignment 2/prng.py
+++ Assignment 2/prng.py
@@ -3,11 +3,16 @@ import random
 
 while True:
     time.sleep(1)
-    with open("prng-service.txt", "a", encoding="utf-8") as f:
+    with open("prng-service.txt", "r+", encoding="utf-8") as f:
         read_data = f.read()
         for line in read_data:
             if line == "run":
+                # generate random number from 1 to 5
                 rand_num = random.randint(1, 5)
                 # erase "run" from prng-services.txt
+
                 # write rand_num to prng-service.txt
+                rand_str = str(rand_num)
+                f.write(rand_str)
+
         # close file - happens automatically because we used 'with'
