8c857b9d4686b6ae5c6fda7c0dce3735a6b5b940
Jessi Frenzel <8403386+starfrenzy@users.noreply.github.com>
2022-06-29 08:34:38 -0500

    Still writing the .py files.

--- Assignment 2/imgsrv.py
+++ Assignment 2/imgsrv.py
@@ -1,8 +1,16 @@
 import time
 
+# read image-service.txt
 while True:
     time.sleep(1)
     with open("image-service.txt", "r+", encoding="utf-8") as f:
         read_data = f.read()
         if read_data.isnumeric():
             our_num = float(read_data)
+
+# erase image-service.txt
+
+# write path to the image
+
+
+#handoff to UI
--- Assignment 2/prng.py
+++ Assignment 2/prng.py
@@ -3,16 +3,20 @@ import random
 
 while True:
     time.sleep(1)
+    # open file
     with open("prng-service.txt", "r+", encoding="utf-8") as f:
         read_data = f.read()
         for line in read_data:
             if line == "run":
                 # generate random number from 1 to 5
                 rand_num = random.randint(1, 5)
+
                 # erase "run" from prng-services.txt
+                f.truncate()  # clears prng-services.txt
+                f.seek(0)  # moves to the beginning of the file
 
                 # write rand_num to prng-service.txt
                 rand_str = str(rand_num)
                 f.write(rand_str)
 
-        # close file - happens automatically because we used 'with'
+    # close file - happens automatically because we used 'with'
